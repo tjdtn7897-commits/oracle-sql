15afe66a4a25e7c32c045a36e4231c22171649a6
kimteayoon <kim@youtu.com>
2026-02-02 10:22:32 +0900

    첫번째 커밋

---
+++ SCOTT.sql
@@ -0,0 +1,8 @@
+-- Oracle (DBMS) - version(21C) - xe(datavase명)
+-- user(scott) - 테이블.
+-- Structured Query Language (SQL)
+SELECT studno, mame -- 칼럼명(전체)
+FROM student; -- 테이블명.
+
+-- 1) professor 테이블. 전체 칼럼 조회.
+select * from professor;
