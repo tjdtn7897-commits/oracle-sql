7204f81dfcfb842b93e6bdd2efb3f98fa36b6927
kimteayoon <kim@youtu.com>
2026-02-02 10:50:08 +0900

    쿼리수정

--- SCOTT.sql
+++ SCOTT.sql
@@ -1,8 +1,13 @@
 -- Oracle (DBMS) - version(21C) - xe(datavase명)
 -- user(scott) - 테이블.
 -- Structured Query Language (SQL)
-SELECT studno, mame -- 칼럼명(전체)
+SELECT studno, name -- 칼럼명(전체)
 FROM student; -- 테이블명.
 
 -- 1) professor 테이블. 전체 칼럼 조회.
 select * from professor;
+
+
+-- 2) student 테이블에 학생번호, 이름, 학년
+select studno, name, grade
+from student;
